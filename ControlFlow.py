a9803a518b271cf3c5e0ac2db3b75cab422fcdac
ishaankaushik91 <ishaankaushik91@gmail.com>
2022-07-27 20:22:18 +0530

    python

---
+++ ControlFlow.py
@@ -0,0 +1,30 @@
+x = 10;
+sum = 0;
+
+# Conditinals
+if x < 10 :
+    print("x is less than 10");
+elif x == 10 :
+    print("x is", 10);
+else :
+    print("x is shit");
+
+
+# Iteration (LOOPS)
+while x < 100 :
+    print(x);
+    x += 1;
+
+while sum < 1000 :
+    sum += x;
+
+print(sum);
+
+# for loop here is to traverse of some iterable thing :
+y = [1, 1, 2, 3, 5, 8, 13];
+summation = 0;
+
+for w in y :
+    summation += w; # w here is = index of y OR
+                    # w = y[0], then y[1], till the end
+print(summation);
---
+++ range.py
@@ -0,0 +1,12 @@
+# The range() function is to generate arithmetic progression
+# NOTE : the number given as end point will never b part of the numbers generated so, numbers generated
+#        will always be n - 1
+
+for i in range(10) :
+    print(i); # 0 - 9
+
+
+for e in range(1, 90) :
+    print(e); # 1 - 89
+
+print(list(range(5, 10))); # Gives a list with numbers in it from 5 - 9
